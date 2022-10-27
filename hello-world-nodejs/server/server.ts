08070f00e5287fd7edd6d381acae957a94f0e3f9
Luis Molina <luis.molina.calizaya@gmail.com>
2022-10-27 13:25:03 -0500

    Dockerfile:se modifica para que sea multistage y obtener un contenedor de tamaño reducido; package.json: se modifica para agregar la linea 8 con script build para transpilar typescript, en linea 9 se cambia el script start para ejecutar el codigo transpilado; tsconfig.json: se modifica la linea 22 agregando la libreria dom y se agrega la linea 24 con la configuracion noImplicitAny: false; server/servert.ts: se agrega el uso de la variable port para el leer el valor PORT de variable de entorno en la linea 3, se modifica la linea 8 para el uso de la varible port y se cambió el ip de 127.0.0.1 a 0.0.0.0, así mismo en la linea 9 se agrega el uso de la variable port

--- hello-world-nodejs/server/server.ts
+++ hello-world-nodejs/server/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
 // Create a new express app instance
+const port = parseInt(process.env.PORT || '3000');
 const app: express.Application = express();
 app.get("/hello", function (req, res) {
   res.send("Hello World!");
 });
-app.listen(3000, "127.0.0.1", function () {
-  console.log("App is listening on port 3000!");
+app.listen(port, "0.0.0.0", function () {
+  console.log(`App is listening on port ${port}!`);
 });
